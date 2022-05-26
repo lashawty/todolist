0a2eff719eccd465c23fe57d2962ee9faebc744a
Sean Coding <104483622+lashawty@users.noreply.github.com>
2022-05-26 16:47:19 +0800

    Update default.js

--- default.js
+++ default.js
@@ -2,12 +2,12 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
-// const filterOption = document.querySelector('.filter-todo');
+const filterOption = document.querySelector('.filter-todo');
 
 //event listeners
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
-// filterOption.addEventListener('click', filterTodo);
+filterOption.addEventListener('click', filterTodo);
 //functions
 
 function addTodo(event) {
